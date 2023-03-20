741d1a08fc5b886f23b43cdd521f89f75c8e2bfd
Abhishek Bajpai <bajpaiabhishek1289@gmail.com>
2023-03-20 15:09:30 +0530

    corrected the header code using bing chat what the fuck

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
-import React, {useEffect} from 'react'
+// import React, {useEffect} from 'react'
+import { useTitle } from './Title'
 
 const Header = ({handleNewGame, wins}) => {
-    useEffect(()=>(document.title = `${wins} wins`),[wins])
-
+  // useEffect(() => (document.title = `${wins} wins`), [wins]);
+  useTitle(`${wins} wins`);
   return (
+
     <header className="header">
         <h4>{wins} wins</h4>
         <h3>start</h3>
